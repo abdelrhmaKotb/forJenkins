7fd801420e0e962874dbe4606272805f6d8f4c41
Abdelrhman Kotb <87202131+abdelrhmaKotb@users.noreply.github.com>
2023-08-17 20:39:36 +0300

    add new test

---
+++ src/test/java/com/example/demo/controllers/TestControllerTest.java
@@ -0,0 +1,13 @@
+package com.example.demo.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestControllerTest {
+
+    @Test
+    void add_two_numbers(){
+        fail("thers is fails here");
+    }
+}
